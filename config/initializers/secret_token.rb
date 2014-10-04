f9c42a74f31e566b4a5d064f56e6daffe018705c
Ashvin Nair <nair.ashvin@gmail.com>
2014-10-04 01:10:20 -0700

    Initial commit Rails set up with mongomapper for ORM working locally

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gradebrain::Application.config.secret_key_base = '6fd0d51d568ee6e462f2120e6b5bfd9d9de84fcb822682174202274a1bbd63755a251e079b82f1acfc3dec5c79ddd6a301278d9418faca3bb854d2d841946a0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gradebrain::Application.config.session_store :cookie_store, key: '_gradebrain_session'
