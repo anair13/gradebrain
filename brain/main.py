d585d8d3b2031beac2f58a01a26f067059032f63
Saavan Patel <saavs94@gmail.com>
2014-10-04 12:08:43 -0700

    Added import code for mongo database to python object

--- brain/main.py
+++ brain/main.py
@@ -1,5 +1,32 @@
+
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-data = client.gradebrain_development.data
-for x in data.find():
-    print(x)
+import os
+def import_data():
+	client = MongoClient('ds043350.mongolab.com', 43350)
+
+	client.gradebraindb.authenticate(
+		os.environ["MONGO_USERNAME"],
+		os.environ["MONGO_PASSWORD"],
+		)
+	data = client.gradebraindb.data
+	x = data.find()
+	grades = []
+	for x in data.find():
+		it = iter(x)
+		if 'semesters' in x['academics']:
+			for y in x["academics"]["semesters"]:
+				if type(y) == dict:
+					if "classes" in y:
+						person = []
+						for course in y["classes"]:
+							if(course["transcript"]):
+								temp = { "class" : course["course_code"]}
+								temp.update(*course["transcript"])
+								person.append(temp)
+							
+					else:
+						raise 
+				if len(person) > 0:
+					grades.append(person)
+	return grades
+print(import_data())
