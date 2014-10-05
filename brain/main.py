2d5d119711ff39b3c3e8767f5d384d919a8c1cf3
Ashvin Nair <nair.ashvin@gmail.com>
2014-10-04 19:02:02 -0700

    making python scripts output back to mongo

--- brain/main.py
+++ brain/main.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
+from linear_model import *
+from internal import *
+from operator import add
 import os
 
 client = MongoClient('ds043350.mongolab.com', 43350)
@@ -6,4 +9,27 @@ client.gradebraindb.authenticate(
 	os.environ["MONGO_USERNAME"],
 	os.environ["MONGO_PASSWORD"],)
 data = client.gradebraindb.data
+models = client.gradebraindb.linear_models
 
+"""
+Write model to the db.gradebraindb.data mongo collection
+class1, class2 are the course code strings, class1 < class2
+linear_model is (b,a) for f = bx + a
+"""
+
+dataset = import_data(data)
+classes = list(set(map(lambda x: x["class"], reduce(add, dataset))))
+
+for class1 in classes:
+    for class2 in classes:
+        if class1 != class2:
+            try:
+                lm = get_lr_classes(dataset, class1, class2)
+                hist = histogram(dataset, "COMPSCI 61A", "COMPSCI 61B")
+                model = {"class1": class1, "class2": class2,
+                    "model": {"t0": lm[1], "t1": lm[0]}, "histogram": hist}
+            except NotEnoughDataException:
+                hist = histogram(dataset, "COMPSCI 61A", "COMPSCI 61B")
+                model = {"class1": class1, "class2": class2, "histogram": hist}
+            models.remove({"class1": class1, "class2": class2})
+            models.insert(model)
--- brain/stats.py
+++ brain/stats.py
@@ -1,3 +1,5 @@
+from internal import *
+
 def covariance(samples):
     """ Gets the covariance of grades """
     xs, ys = list(map(lambda a: a[0], samples)), list(map(lambda a: a[1], samples))
@@ -12,17 +14,19 @@ def variance(samples):
     sum([(ys[i] - (xs[i] * b + a)) ** 2 for i in range(len(samples))]) / len(samples)
 
 def simple_lr(samples):
-    """Returns linear regression coefficients (b, a) where f = bx + a
+    """ Returns linear regression coefficients (b, a) where f = bx + a
     samples :: [(Float, Float)]
 
     Throws a NotEnoughDataException if not enough data exists.
     """
     xs, ys = list(map(lambda a: a[0], samples)), list(map(lambda a: a[1], samples))
+    if len(xs) == 0:
+        raise NotEnoughDataException("Not enough data! Regression model has a coefficient of 0 or infinity")
     avgx = sum(xs) / len(xs)
     avgy = sum(ys) / len(ys)
     bnum = sum([(xs[i] - avgx) * (ys[i] - avgy) for i in range(len(samples))])
     bdenom = sum([(xs[i] - avgx) ** 2 for i in range(len(samples))])
-    if bnum == 0 or bdenum == 0:
+    if bnum == 0 or bdenom == 0:
         raise NotEnoughDataException("Not enough data! Regression model has a coefficient of 0 or infinity")
     b = bnum / bdenom
     a = avgy - b * avgx
